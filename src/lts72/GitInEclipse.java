ccbfebaccfa2ff6321ebdb47100b8891fcf10ce1
nthiagu <nthiagu@gmail.com>
2016-07-31 12:04:56 -0700

    second change by Thiagu

--- src/lts72/GitInEclipse.java
+++ src/lts72/GitInEclipse.java
@@ -6,6 +6,8 @@ public class GitInEclipse {
 	
 	{
 		System.out.println("Hello World for CMPE 202 by Thiagu.....Snehal MAde changes ");
+		System.out.println("Second change for CMPE 202 by Thiagu..... ");
+
 	}
 
 }
